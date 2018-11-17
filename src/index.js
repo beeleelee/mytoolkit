71ca78e86ebb1289ae08dd3aee5a2ec46ac6524b
lifeng <beeleelee@outlook.com>
2018-11-17 13:23:38 +0800

    connect all files width index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import currentTime from './currentTime'
+import tick from './tick'
+import Tween from './tween'
+import Deceleration from './deceleration'
+
+export {
+  currentTime,
+  tick,
+  Tween,
+  Deceleration,
+}
