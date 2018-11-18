a341a3b4250d9183f3c63f4162e6140ae955eceb
lifeng <beeleelee@outlook.com>
2018-11-18 21:12:31 +0800

    kit fillZero

--- src/fillZero.js
+++ src/fillZero.js
@@ -1,5 +1,16 @@
 import typeOf from './typeOf'
 
 export default function fillZero(num, size = 2) {
+  let numberToFill = parseFloat(num)
+  if(typeOf(numberToFill) !== 'Number'){
+    throw new TypeError('fillZero needs number for its first parameter!')
+  }
+
+  numberToFill = String(numberToFill)
+  let fillSize = size - numberToFill.length
+  if(fillSize <= 0){
+    return numberToFill
+  }
   
+  return Array.from({length: fillSize}).map(item => 0).join('') + numberToFill
 }
