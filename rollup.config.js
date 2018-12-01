509eef0c1404fc2bf5d0594f2bdfbf653e52f6a0
lifeng <beeleelee@outlook.com>
2018-12-01 13:02:24 +0800

    add ease option for tween

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,51 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
+import {
+  uglify
+} from 'rollup-plugin-uglify'
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/index.js',
-		output: {
-			name: 'mytoolkit',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'mytoolkit',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
       resolve(), // so Rollup can find `ms`
       babel({
         exclude: 'node_modules/**'
       }),
       commonjs(), // so Rollup can convert `ms` to an ES module
       uglify()
-		]
-	},
+    ]
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/index.js',
-		external: [],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-    ],
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify 
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    external: ['bezier-easing'],
+    output: [{
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }],
     plugins: [
       babel({
         exclude: 'node_modules/**'
       }),
     ]
-	}
+  }
 ];
--- test/tween.test.js
+++ test/tween.test.js
@@ -1,15 +1,14 @@
 import test from 'ava'
 const mytoolkit = require('../dist/mytoolkit.cjs')
 const {
-  typeOf,
-  Tween 
-} = mytoolkit 
+  Tween
+} = mytoolkit
 
 test('Tween', t => {
   let tween = new Tween({
     duration: 500,
-    onStep: (tw, p) => {
-      console.log(p)
+    onStep: () => {
+
     }
   })
   tween.start()
