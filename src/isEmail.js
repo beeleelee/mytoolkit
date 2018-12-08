463b9a1c2e5875efeadb91a0bafa5de8e1afa1f8
lifeng <beeleelee@outlook.com>
2018-12-08 21:48:24 +0800

    add comment

--- src/isEmail.js
+++ src/isEmail.js
@@ -1,6 +1,12 @@
-export const isEmail = (emailAdress) => {
+/**
+ * @desc check the format for the input email 
+ * 
+ * @params {String} emailAddress - a email address
+ * @returns {Boolean} - true | false 
+ */
+export const isEmail = (emailAddress) => {
   let emailRE = /^(?:[-\w])+(?:\.[-\w]+)*@(?:[-\w])+(?:(?:\.[-\w]+)+)$/
-  if (emailAdress && emailRE.test(emailAdress)) {
+  if (emailAddress && emailRE.test(emailAddress)) {
     return true
   }
 
--- src/noop.js
+++ src/noop.js
@@ -1,2 +1,5 @@
+/**
+ * @desc a empty function doing nothing when called
+ */
 export function noop() { }
 export default noop 
