1fc02a42295b1fa906e209b45a5500615ac290af
lifeng <beeleelee@outlook.com>
2018-12-09 10:44:07 +0800

    kit uniq

---
+++ src/uniq.js
@@ -0,0 +1,16 @@
+import typeOf from './typeOf'
+
+/**
+ * 
+ * @param {Array} array - a simple array
+ * @returns {Array} - return a new array without doubled values
+ */
+export function uniq(array) {
+  if (typeOf(array) !== 'Array') {
+    throw new TypeError(`unexpected type of ${typeOf(array)}, expected Array}`)
+  }
+
+  return [...new Set(...array)]
+}
+
+export default uniq 
