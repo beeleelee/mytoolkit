9e94b32262aa681c28edf81063f41478e3366a54
lifeng <beeleelee@outlook.com>
2018-12-10 09:30:43 +0800

    kit randInt comment

--- src/randInt.js
+++ src/randInt.js
@@ -1,14 +1,18 @@
 import typeOf from './typeOf.js'
-
+/**
+ * @param {Number} min - a simple int 
+ * @param {Number} max - a simple int
+ * @returns {Number} - a random int between min and max, maybe include min but not max
+ */
 export function randInt(min, max) {
   min = parseInt(min)
   max = parseInt(max)
-  if(typeOf(min) !== 'Number'){
+  if (typeOf(min) !== 'Number') {
     throw new TypeError('randInt needs number from its parameter!')
   }
 
-  if(!max){
-    max = min 
+  if (!max) {
+    max = min
     min = 0
   }
   return Math.floor(Math.random() * (max - min) + min)
