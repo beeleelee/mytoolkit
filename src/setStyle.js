06b9cf6b3c7c0f57871875e1f95ddc7d3fe480f8
lifeng <beeleelee@outlook.com>
2018-12-10 09:48:02 +0800

    add comment

--- src/setStyle.js
+++ src/setStyle.js
@@ -1,38 +1,60 @@
 import typeOf from './typeOf'
 
-let styleKeys = null 
+let styleKeys = null
 
+/**
+ * @ignore
+ */
 const setSingleStyle = (ele, name, value) => {
-  if(!ele || !ele.style){
+  if (!ele || !ele.style) {
     throw new Error('missing HTML node element for setStyle!')
   }
   name = checkVendorPrefix(name)
-  if(typeOf(name) !== 'Undefined' && typeOf(value) !== 'Undefined'){
+  if (typeOf(name) !== 'Undefined' && typeOf(value) !== 'Undefined') {
     ele.style[name] = value
   }
 }
 
+/**
+ * @param {HTMLElement} ele - dom element 
+ * @param {String} name - the style property name, it also can be an object to set multiple style properties
+ * @param {String|Number} value - the value of the style property
+ * 
+ * @example
+ * 
+ * setStyle(document.body, 'fontSize', '16px')
+ * setStyle(doucment.body, {
+ *  backgroundColor: '#ffffff',
+ *  minHeight: '100vh'
+ * })
+ */
 export const setStyle = (ele, name, value) => {
-  if(typeOf(name) === 'Object') {
+  if (typeOf(name) === 'Object') {
     Object.keys(name)
       .map(n => {
         setSingleStyle(ele, n, name[n])
       })
-  }else{
+  } else {
     setSingleStyle(ele, name, value)
   }
 }
 
+/**
+ * @ignore
+ */
 function checkVendorPrefix(name) {
   let styleKeys = getStyleKeys()
 
   return styleKeys[name] || name
 }
 
+/**
+ * @ignore
+ */
 function getStyleKeys() {
-  if(styleKeys !== null) return styleKeys 
+  if (styleKeys !== null) return styleKeys
 
-  if(typeof document === 'undefined'){
+  if (typeof document === 'undefined') {
     throw new Error('missing document!')
   }
 
@@ -42,8 +64,8 @@ function getStyleKeys() {
   )
   styleKeys = keys.reduce((res, key) => {
     let name = key.replace(/^webkit(\w)/, (a, s) => s.toLowerCase())
-    res[name] = key 
-    return res 
+    res[name] = key
+    return res
   }, {})
 
   return styleKeys
