273d019eb5fac4609918ac327e2964c1f81bf7dc
lifeng <beeleelee@outlook.com>
2018-12-10 10:34:25 +0800

    kit dashToCamel add test

--- src/dashToCamel.js
+++ src/dashToCamel.js
@@ -5,7 +5,10 @@ export function dashToCamel(dashStr) {
   if (typeOf(dashStr) !== 'String') {
     throw new TypeError(`dashToCamel expected a dashStr, but got ${typeOf(dashStr)}`)
   }
-  let camelStr = dashStr.toLowerCase()
+  if (dashStr.indexOf('-') === -1) {
+    return dashStr
+  }
+  let camelStr = dashStr
   camelStr = trim(camelStr)
   // remove the dash at begin or end
   camelStr = camelStr.replace(/^-|-$/g, '')
---
+++ test/dashToCamel.test.js
@@ -0,0 +1,12 @@
+import test from 'ava'
+const mytoolkit = require('../dist/mytoolkit.cjs')
+const {
+  dashToCamel
+} = mytoolkit
+
+test('dashToCamel', t => {
+  t.is(dashToCamel('-webkit-transform-'), 'webkitTransform')
+  t.is(dashToCamel('font-size'), 'fontSize')
+  t.is(dashToCamel('align-items'), 'alignItems')
+  t.is(dashToCamel('textAlign'), 'textAlign')
+})
