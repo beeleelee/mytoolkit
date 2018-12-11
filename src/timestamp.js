d97486e7b68feaee849c5763bf25cb7c9e69bfeb
lifeng <beeleelee@outlook.com>
2018-12-11 09:00:07 +0800

    add comment

--- src/timestamp.js
+++ src/timestamp.js
@@ -1,5 +1,9 @@
 import currentTime from './currentTime'
 
+/**
+ * @desc - like currentTime but its seconds not miniseconds
+ * @returns - seconds since 00:00:00 UTC on January 1, 1970
+ */
 export function timestamp() {
   return Math.round(currentTime() / 1000)
 }
