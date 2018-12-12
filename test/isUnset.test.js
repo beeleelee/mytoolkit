d18e35132b20e45fdc6dc587e3712ed1a16f02c4
lifeng <beeleelee@outlook.com>
2018-12-12 09:58:26 +0800

    kit isUnset test

---
+++ test/isUnset.test.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+const mytoolkit = require('../dist/mytoolkit.cjs')
+const {
+  isUnset
+} = mytoolkit
+
+test('isSet', t => {
+  const obj = {
+    name: 'obj',
+    type: null
+  }
+  t.false(isUnset(obj.name))
+  t.true(isUnset(obj.id))
+  t.true(isUnset(obj.type))
+})
