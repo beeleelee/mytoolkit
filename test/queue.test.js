a04e3928f4b604a7bc7a8df59da9731a4484dbfb
lifeng <beeleelee@outlook.com>
2018-12-14 13:47:24 +0800

    add queue test

---
+++ test/queue.test.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+const mytoolkit = require('../dist/mytoolkit.cjs')
+const {
+  Queue
+} = mytoolkit
+
+test('queue', t => {
+  let q = new Queue()
+  t.is(q.dequeue(), undefined)
+  q.enqueue(1)
+  t.is(q.dequeue(), 1)
+  q.enqueue({ a: 2 })
+    .enqueue(3)
+    .enqueue([4])
+    .enqueue(a => a)
+  t.deepEqual(q.dequeue(), { a: 2 })
+  t.is(q.dequeue(), 3)
+  t.deepEqual(q.dequeue(), [4])
+  t.is(q.dequeue()(5), 5)
+})
