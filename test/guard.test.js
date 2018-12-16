2cc7b6d391154edf399dbb06048740b3d1114d3c
lifeng <beeleelee@outlook.com>
2018-12-16 15:45:17 +0800

    guard test

---
+++ test/guard.test.js
@@ -0,0 +1,16 @@
+import test from 'ava'
+const mytoolkit = require('../dist/mytoolkit.cjs')
+const {
+  guard,
+  isSet
+} = mytoolkit
+
+test('guard', t => {
+  const guardZero = guard(value => value !== 0, 1)
+  const guardNull = guard(value => value !== null, [])
+  const guardUnset = guard(value => isSet(value), {})
+
+  t.is(guardZero(0), 1)
+  t.deepEqual(guardNull(null), [])
+  t.deepEqual(guardUnset(), {})
+})
