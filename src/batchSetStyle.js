a28c7bf7054622f721231c4c1eabb131ba49b1d6
lifeng <beeleelee@outlook.com>
2018-12-19 09:22:05 +0800

    works on batchSetStyle

--- src/batchSetStyle.js
+++ src/batchSetStyle.js
@@ -7,3 +7,12 @@
  *  3. lock the style setting when doing a task, put incoming task into a queue
  *  4. use tick as setting frame, check remaining task at every frame
  */
+import setStyle from './setStyle'
+import delay from './delay'
+import queue from './queue'
+
+export function batchSetStyle(element, name, value) {
+
+}
+
+export default batchSetStyle
