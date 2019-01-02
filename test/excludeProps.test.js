f181b46518069041e6a263f9899f64c16fc7ff98
lifeng <beeleelee@outlook.com>
2019-01-02 16:37:21 +0800

    fix typo

--- test/excludeProps.test.js
+++ test/excludeProps.test.js
@@ -4,7 +4,7 @@ const {
   excludeProps
 } = mytoolkit
 
-test('echo', t => {
+test('excludeProps', t => {
   const o = { a: 1, b: 2, c: 3, d: 4 }
   t.deepEqual(excludeProps(o, 'a'), { b: 2, c: 3, d: 4 })
   t.deepEqual(excludeProps(o, ['a', 'd']), { b: 2, c: 3 })
--- test/selectProps.test.js
+++ test/selectProps.test.js
@@ -4,7 +4,7 @@ const {
   selectProps
 } = mytoolkit
 
-test('echo', t => {
+test('selectProps', t => {
   const o = { a: 1, b: 2, c: 3, d: 4 }
   t.deepEqual(selectProps(o, 'a'), { a: 1 })
   t.deepEqual(selectProps(o, ['a', 'd']), { a: 1, d: 4 })
