49d79432bc13f41205236b9686a82ea51634ddf1
lifeng <beeleelee@outlook.com>
2019-01-15 20:50:48 +0800

    kit quickSort add comment

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,6 +1,11 @@
 import typeOf from './typeOf'
 import head from './head'
 
+/**
+ * 
+ * @param {Array} list - a simple array
+ * @param {Function} func - a compare function
+ */
 export function quickSort(list, func = compare) {
   if (typeOf(list) !== 'Array') {
     throw new TypeError('expected param to be an Array! but got a type of ' + typeOf(list))
