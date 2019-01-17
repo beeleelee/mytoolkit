1b87e38b586a60ad1caa4f5a5496189eb28bddb1
lifeng <beeleelee@outlook.com>
2019-01-17 21:50:02 +0800

    kit largest add comments

--- src/nlargest.js
+++ src/nlargest.js
@@ -1,6 +1,12 @@
 import typeOf from './typeOf'
 import quickSort from './quickSort'
 
+/**
+ * 
+ * @param {Array} list - a simple array
+ * @param {Number} num - the size of the largest list
+ * @return {Array} - the larget list
+ */
 export function nlargest(list, num = 1) {
   if (typeOf(list) !== 'Array') {
     throw new TypeError(`expect param to be type of Array, but got ${typeOf(list)}`)
