b72a8c378c47b947f98310a606178248600ae796
lifeng <beeleelee@outlook.com>
2019-01-23 21:59:25 +0800

    kit queryJoin add comments

--- src/queryJoin.js
+++ src/queryJoin.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {String} left - a url or query string
+ * @param {String} right - a query string
+ * @returns {String} - a combined string
+ */
 export function queryJoin(left, right) {
   let joinCharacter
   let alreadHasQuery = /[%\w]+=/.test(left)
