9c74213ba3c928bca5d54baf6330f886c8d9cce1
lifeng <beeleelee@outlook.com>
2019-01-24 21:44:28 +0800

    kit tail add comments

--- src/tail.js
+++ src/tail.js
@@ -1,5 +1,11 @@
 import typeOf from './typeOf'
 
+/**
+ * 
+ * @param {Array} list - a simple array
+ * @param {Number} size - the size of the trancated list
+ * @return {Any | Array} - the trancated list or the last item 
+ */
 export function tail(list, size = 1) {
   if (typeOf(list) !== 'Array') {
     throw new TypeError('expected param to be an Array! but got a type of ' + typeOf(list))
