2401dbf9c66ad39414215e55b484879777422745
lifeng <beeleelee@outlook.com>
2019-03-31 10:42:40 +0800

    kit extend add test

---
+++ test/extend.test.js
@@ -0,0 +1,18 @@
+const test = require('tape')
+const mytoolkit = require('../dist/mytoolkit.cjs')
+const {
+  extend
+} = mytoolkit
+
+test('extend', t => {
+  const o = { a: 1, b: 2, c: 3, d: 4 }
+  const a = { a: 'a', b: 'b', c: 'c', d: 'd' }
+  const b = { m: 5, n: 6 }
+  const c = { x: 'x', z: 'z' }
+  t.deepEqual(extend({}, o, b), { a: 1, b: 2, c: 3, d: 4, m: 5, n: 6 })
+  t.deepEqual(extend({}, a, c), { a: 'a', b: 'b', c: 'c', d: 'd', x: 'x', z: 'z' })
+  t.deepEqual(extend({}, o, a), { a: 'a', b: 'b', c: 'c', d: 'd' })
+  t.deepEqual(extend(o, a), { a: 'a', b: 'b', c: 'c', d: 'd' })
+
+  t.end()
+})
