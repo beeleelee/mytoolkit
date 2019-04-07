f4caeb92161602b91029e56ba58192c1c3072da0
lifeng <beeleelee@outlook.com>
2019-04-07 09:43:30 +0800

    update

--- test/encodeJSON.test.js
+++ test/encodeJSON.test.js
@@ -8,6 +8,9 @@ const {
 test('encodeJSON', t => {
   t.is(encodeJSON([1, 2, 3]), '[1,2,3]')
   t.is(encodeJSON({ a: 1, b: 2, c: 3 }), '{"a":1,"b":2,"c":3}')
+  t.is(encodeJSON(true), 'true')
+  t.is(encodeJSON(''), '""')
+  t.is(encodeJSON(null), 'null')
 
   t.end()
 })
@@ -15,6 +18,7 @@ test('encodeJSON', t => {
 test('decodeJSON', t => {
   t.deepEqual(decodeJSON('[1,2,3]'), [1, 2, 3])
   t.deepEqual(decodeJSON('{"a":1,"b":2,"c":3}'), { a: 1, b: 2, c: 3 })
+  t.is(decodeJSON('""'), '')
 
   t.end()
 })
